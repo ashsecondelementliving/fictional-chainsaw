ca6f9ab6352dc3214fcadaf385119a4833a69848
ashsecondelementliving <ash@secondelementliving.com>
2025-06-16 01:00:01 -0400

    Update FormatLead.py -- added tenure and company fields to file

--- FormatLead.py
+++ FormatLead.py
@@ -8,7 +8,7 @@ def formatLeadpy(input_file):
             reader = csv.DictReader(infile)
             reader.fieldnames = [name.lstrip('\ufeff') if name else name for name in reader.fieldnames]
 
-            fieldnames = ['Profile ID', 'LinkedIn Link', 'Name','Email Address', 'Experiences', 'About']
+            fieldnames = ['Profile ID', 'LinkedIn Link', 'Name','Email Address','Tenure At Position','Company', 'Experiences', 'About']
 
             with open(output_file, mode='w', encoding='utf-8', newline='') as outfile:
                 writer = csv.DictWriter(outfile, fieldnames=fieldnames)
@@ -27,11 +27,15 @@ def formatLeadpy(input_file):
                         linkedin_link = row.get('ProfileUrl', '')
                         profile_id = row.get('ProfileId', '')
                         email_address = row.get('EmailAddress')
+                        tenure = row.get('Tenure At Position')
+                        company = row.get('Company')
                         name = f"{row.get('FirstName', '').strip()} {row.get('LastName', '').strip()}"
                     else:
                         linkedin_link = row.get('profile_link', '')
                         profile_id = row.get('id', '')
                         email_address = row.get('email')
+                        tenure = ''
+                        company = ''
                         name = f"{row.get('first_name', '').strip()} {row.get('last_name', '').strip()}"
 
                     writer.writerow({
@@ -39,6 +43,8 @@ def formatLeadpy(input_file):
                         'LinkedIn Link': linkedin_link,
                         'Name': name,
                         'Email Address': email_address,
+                        'Tenure At Position': tenure,
+                        'Company': company,
                         'Experiences': '',
                         'About': ''
                     })
