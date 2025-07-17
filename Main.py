e65723f2362f9c14b9262732c6d8a25e11f86240
ashsecondelementliving <ash@secondelementliving.com>
2025-07-16 23:32:42 -0400

    Update Main.py- update prompt

--- Main.py
+++ Main.py
@@ -40,7 +40,7 @@ In between the quotation marks should be a subject line for the message that you
 
 Then create the paragraph: "Hi (first name),"
 
-Then create 1 condensed sentence (that is grammatically correct and not a run-on) that should be formatted like: “I saw ..., your...” outlining career progress and achievements, then mention their positive attributes.
+Then create 1 condensed sentence (that is grammatically correct and not a run-on) that should be formatted like: “I saw ..., your...” outlining career progress and achievements, then mention their positive attributes. This sentence will be dense with minimal to no filler words.
 
 Then add: "Are you achieving your career goals at (current company name)?"
 
