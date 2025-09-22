8906d21cd31aec39b20e0c17ad9844854b57b44d
ashsecondelementliving <ash@secondelementliving.com>
2025-09-22 19:32:07 -0400

    Update mapper.py - add output file as argument

--- mapper.py
+++ mapper.py
@@ -7,7 +7,7 @@ def mapperMain(senderName, input_file, output_file, socketio=None):
         reader = csv.DictReader(infile)
         if "vmid" in reader.fieldnames:
             print("Auto Process")
-            creatorMain(input_file, senderName, socketio)
+            creatorMain(input_file, senderName, output_file, socketio)
         elif "About" in reader.fieldnames:
             print("Manual Process")
             mainpy(senderName, input_file, output_file, socketio)
